015b12f20538ba4976de37e5a3fed43dc310b495
Claudio Raimondi <craimond@c1r5p1.42firenze.it>
2024-04-22 18:58:59 +0200

    Add comment

--- cpp04/ex02/Animal.hpp
+++ cpp04/ex02/Animal.hpp
@@ -6,7 +6,7 @@
 /*   By: craimond <bomboclat@bidol.juis>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 16:13:29 by craimond          #+#    #+#             */
-/*   Updated: 2024/04/22 18:54:59 by craimond         ###   ########.fr       */
+/*   Updated: 2024/04/22 18:58:48 by craimond         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,8 @@ class Animal
 		Animal(const Animal& other); //copy constructor
 		Animal &operator=(const Animal& other); //copy assignment operator
 		virtual ~Animal();
+		//VIRTUAL: il metodo PUO' essere ridefinito nelle classi figlie
+		//VIRTUAL PURO ( = 0): il metodo DEVE essere ridefinito nelle classi figlie (rendendo quindi la classe astratta) 
 		virtual void makeSound(void) const = 0; //const: non modifica le member variables della classe su cui 'e chiamata
 		std::string getType(void) const;
 	protected:
